e72e95f3d85831e750c1d93bec8c7b0f2cf0a32d
Craig Campbell <iamcraigcampbell@gmail.com>
2013-02-06 22:09:00 -0500

    Increase development version to 1.3.0

--- mousetrap.js
+++ mousetrap.js
@@ -16,7 +16,7 @@
  * Mousetrap is a simple keyboard shortcut library for Javascript with
  * no external dependencies
  *
- * @version 1.2.2
+ * @version 1.3.0
  * @url craig.is/killing/mice
  */
 (function() {
