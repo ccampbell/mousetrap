03367b8428454a4a3be554cef9c7f60743bfabc6
Craig Campbell <iamcraigcampbell@gmail.com>
2013-05-14 01:15:20 -0400

    On second thought let's call this 1.4

--- mousetrap.js
+++ mousetrap.js
@@ -17,7 +17,7 @@
  * Mousetrap is a simple keyboard shortcut library for Javascript with
  * no external dependencies
  *
- * @version 1.3.3
+ * @version 1.4.0
  * @url craig.is/killing/mice
  */
 (function() {
