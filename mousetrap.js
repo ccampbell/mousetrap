e677150c165cfcd9711e7fc8a7cf8b919c9582b0
Kenneth Blomqvist <kblomqvist@rallydev.com>
2013-07-02 15:33:34 +0300

    Recognize contenteditable plaintext-only

--- mousetrap.js
+++ mousetrap.js
@@ -892,7 +892,7 @@
             }
 
             // stop for input, select, and textarea
-            return element.tagName == 'INPUT' || element.tagName == 'SELECT' || element.tagName == 'TEXTAREA' || (element.contentEditable && element.contentEditable == 'true');
+            return element.tagName == 'INPUT' || element.tagName == 'SELECT' || element.tagName == 'TEXTAREA' || (element.contentEditable && (element.contentEditable == 'true' || element.contentEditable == 'plaintext-only'));
         },
 
         /**
