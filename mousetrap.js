c137f5286dc27155e1b265c7554223cdd74fdf23
Anil Kulkarni <anil@textio.com>
2016-07-13 03:04:51 -0700

    Respect the passed in action
    If the caller specifies an action to use, then respect that choice.
    That allows for callers to pick up the keypress events that browsers
    sometimes fire even when control keys are in use

--- mousetrap.js
+++ mousetrap.js
@@ -344,12 +344,12 @@
         // that we think would work best for this key
         if (!action) {
             action = _getReverseMap()[key] ? 'keydown' : 'keypress';
-        }
 
-        // modifier keys don't work as expected with keypress,
-        // switch to keydown
-        if (action == 'keypress' && modifiers.length) {
-            action = 'keydown';
+            // modifier keys don't work as expected with keypress,
+            // switch to keydown
+            if (action == 'keypress' && modifiers.length) {
+                action = 'keydown';
+            }
         }
 
         return action;
