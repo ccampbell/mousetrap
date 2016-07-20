cbfba0da91ff4d11766d320e89060d24f9c72944
Anil Kulkarni <anil@textio.com>
2016-07-19 20:00:45 -0700

    Stop keypresses from firing when the modifiers don't match
    Now that keypresses are allowed, the code needs to always check to make
    sure the modifiers match before accepting the callback.

--- mousetrap.js
+++ mousetrap.js
@@ -569,14 +569,10 @@
                     continue;
                 }
 
-                // if this is a keypress event and the meta key and control key
-                // are not pressed that means that we need to only look at the
-                // character, otherwise check the modifiers as well
-                //
                 // chrome will not fire a keypress if meta or control is down
                 // safari will fire a keypress if meta or meta+shift is down
                 // firefox will fire a keypress if meta or control is down
-                if ((action == 'keypress' && !e.metaKey && !e.ctrlKey) || _modifiersMatch(modifiers, callback.modifiers)) {
+                if (_modifiersMatch(modifiers, callback.modifiers)) {
 
                     // when you bind a combination or sequence a second time it
                     // should overwrite the first one.  if a sequenceName or
